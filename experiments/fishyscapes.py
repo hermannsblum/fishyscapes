8bf565546474eaf0250cab974e1b512fe10cf5d3
Giancarlo <giancarlo.dibiase@advertima.com>
2020-07-17 08:44:29 +0200

    added function for testing synthesis models

--- experiments/fishyscapes.py
+++ experiments/fishyscapes.py
@@ -12,6 +12,7 @@ from .utils import ExperimentData
 from fs.data.fsdata import FSData
 from fs.data.utils import load_gdrive_file
 from fs.data.augmentation import crop_multiple
+from driving_uncertainty.test_fishy_torch import AnomalyDetector
 
 ex = Experiment()
 ex.capture_out_filter = apply_backspaces_and_linefeeds
@@ -61,7 +62,46 @@ def saved_model(testing_dataset, model_id, _run, _log, batching=False, validatio
     fs = bdlb.load(benchmark="fishyscapes", download_and_prepare=False)
     _run.info['{}_anomaly'.format(model_id)] = fs.evaluate(eval_func, data)
 
-
+@ex.command
+def resynthesis_model(testing_dataset, _run, _log, ours=True,  validation=False):
+    fsdata = FSData(**testing_dataset)
+    
+    # Hacks because tf.data is shit and we need to translate the dict keys
+    def data_generator():
+        dataset = fsdata.validation_set if validation else fsdata.testset
+        for item in dataset:
+            data = fsdata._get_data(training_format=False, **item)
+            out = {}
+            for m in fsdata.modalities:
+                blob = crop_multiple(data[m])
+                if m == 'rgb':
+                    m = 'image_left'
+                if 'mask' not in fsdata.modalities and m == 'labels':
+                    m = 'mask'
+                out[m] = blob
+            yield out
+    
+    data_types = {}
+    for key, item in fsdata.get_data_description()[0].items():
+        if key == 'rgb':
+            key = 'image_left'
+        if 'mask' not in fsdata.modalities and key == 'labels':
+            key = 'mask'
+        data_types[key] = item
+    
+    data = tf.data.Dataset.from_generator(data_generator, data_types)
+    
+    detector = AnomalyDetector(ours=ours)
+    
+    fs = bdlb.load(benchmark="fishyscapes", download_and_prepare=False)
+    
+    if ours:
+        model_id = 'synthesis_boosting'
+    else:
+        model_id = 'resynthesis'
+        
+    _run.info['{}_anomaly'.format(model_id)] = fs.evaluate(detector.estimator, data)
+    
 if __name__ == '__main__':
     ex.run_commandline()
     os._exit(os.EX_OK)
