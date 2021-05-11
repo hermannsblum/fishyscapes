d1e5ebbace0da041dbea692be93cc08c28ac8161
Hermann <blumh@ethz.ch>
2021-05-11 10:01:46 +0200

    add dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from setuptools import setup
 setup(name='fs',
       version="0.0",
       install_requires=[
-          'bdlb @ git+https://github.com/hermannsblum/bdl-benchmark.git'
+          'bdlb @ git+https://github.com/hermannsblum/bdl-benchmark.git',
+          'gdown',
+          'sacred',
+          'pymongo',
+          'imgaug',
+          'tensorflow-datasets==3.1.0',
       ],
       packages=['fs', 'fs.data'])
