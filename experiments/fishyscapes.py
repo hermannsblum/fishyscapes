81c70858e25784e91092a30f264d240374bcef4c
Hermann <blumh@ethz.ch>
2021-10-07 11:10:42 +0200

    initial code rts

--- experiments/fishyscapes.py
+++ experiments/fishyscapes.py
@@ -62,6 +62,48 @@ def saved_model(testing_dataset, model_id, _run, _log, batching=False, validatio
     fs = bdlb.load(benchmark="fishyscapes", download_and_prepare=False)
     _run.info['{}_anomaly'.format(model_id)] = fs.evaluate(eval_func, data)
 
+@ex.command
+def real_time_sigmoid(testing_dataset, _run, _log, validation=False):
+    sys.path.insert(0, os.path.join(os.getcwd(), os.path.dirname(os.path.dirname(__file__)), 'rts', 'network'))
+    from network.real_time_sigmoid import Real_Time_Sigmoid
+    rts = Real_Time_Sigmoid()
+
+    fsdata = FSData(**testing_dataset)
+
+    # Hacks because tf.data is shit and we need to translate the dict keys
+    def data_generator():
+        dataset = fsdata.validation_set if validation else fsdata.testset
+        for item in dataset:
+            data = fsdata._get_data(training_format=False, **item)
+            out = {}
+            for m in fsdata.modalities:
+                blob = crop_multiple(data[m])
+                if m == 'rgb':
+                    m = 'image_left'
+                if 'mask' not in fsdata.modalities and m == 'labels':
+                    m = 'mask'
+                out[m] = blob
+            yield out
+
+    data_types = {}
+    for key, item in fsdata.get_data_description()[0].items():
+        if key == 'rgb':
+            key = 'image_left'
+        if 'mask' not in fsdata.modalities and key == 'labels':
+            key = 'mask'
+        data_types[key] = item
+
+    data = tf.data.Dataset.from_generator(data_generator, data_types)
+
+    fs = bdlb.load(benchmark="fishyscapes", download_and_prepare=False)
+
+    def wrapper(image):
+        image = image.numpy().astype('uint8')
+        anomaly_score = rts.eval(image)
+
+        return anomaly_score
+
+    _run.info['real_time_sigmoid_anomaly'] = fs.evaluate(wrapper, data)
 
 @ex.command
 def entropy_maximization(testing_dataset, _run, _log, validation=False):
