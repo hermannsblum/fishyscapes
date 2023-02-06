bc9eb4da7432bae75807c7f9c54395e248059880
4PiR2 <19995777+4PiR2@users.noreply.github.com>
2023-02-06 16:46:10 +0000

    test

--- main.py
+++ main.py
@@ -1,59 +1,22 @@
+import json
 import os
-import subprocess
-import sys
 
-
-def run(cmd, cwd=None, env=None):
-    print(f'>>> {cwd} $ {" ".join(cmd)}')
-    p = subprocess.Popen(cmd, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                         cwd=cwd, env=env, universal_newlines=True, bufsize=1)
-    os.set_blocking(p.stdout.fileno(), False)
-    os.set_blocking(p.stderr.fileno(), False)
-    lines = []
-    lines_out = []
-    lines_err = []
-    while True:
-        rc = p.poll()
-        while line_out := p.stdout.readline():
-            lines.append(line_out)
-            lines_out.append(line_out)
-            print(line_out, end='')
-        while line_err := p.stderr.readline():
-            lines.append(line_err)
-            lines_err.append(line_err)
-            print(line_err, end='')
-        if rc is not None:
-            break
-    print('EXIT CODE:', rc)
-    return rc, ''.join(lines), ''.join(lines_out), ''.join(lines_err)
+from utils import run
 
 
 def main():
-    print(sys.argv)
-    print(sys.version)
-    run(['which', 'singularity'])
-    run(['pwd'])
-    run(['cat', '/proc/cpuinfo'])
-    run(['free'])
-    run(['nvidia-smi'])
-    run(['nvidia-smi', '-L'])
-    exit(0)
+    with open('settings.json', 'r') as f:
+        settings = json.load(f)
 
-    run(['mkdir', '-p', '/tmp/results'])
-    run(['rm', '-rf', '/tmp/results/*'])
+    run(['mkdir', '-p', settings['tmp_results_path']])
+    run(['rm', '-rf', os.path.join(settings['tmp_results_path'], '*')])
     cmd = [
-        'singularity', 'run', '--nv', '--pwd', '/workspace/synboost',
-        '--bind', '/tmp/results:/workspace/synboost/results',
-        'synboost_1.0.sif',
+        'singularity', 'run', '--nv', '--pwd', settings['run']['pwd'],
+        '--bind', f"{settings['tmp_results_path']}:{settings['run']['results_path']}",
+        settings['sif_path'],
     ]
     run(cmd, '/submitted_containers')
-    cmd2 = [
-        'python3', 'eval.py',
-    ]
-    run(cmd2, '/submitted_containers')
-    pass
 
 
 if __name__ == '__main__':
     main()
-    print(sys.argv)
