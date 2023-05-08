d475a04ac91afec344e66a5c9e441ba979cfe762
Hermann <5478501+hermannsblum@users.noreply.github.com>
2023-05-08 10:33:18 +0200

    debugging

--- main.py
+++ main.py
@@ -27,6 +27,15 @@ def main():
     run(['chmod', '777', settings['tmp_pred_path']])
     run(' '.join(['rm', '-rf', os.path.join(settings['tmp_pred_path'], '*')]), shell=True)
     cmd = [
+        'singularity', 'exec', '--nv', '-u',
+        '--bind', f"{settings['tmp_pred_path']}:/output,"
+                  f"{settings['val_rgb_path']}:/input",
+        os.path.join('/tmp', f'fishyscapes_pr_{pr_id}.simg'),
+        'whoami'
+    ]
+    try:
+        run(['runuser', '-l', 'blumh', '-c', ' '.join(cmd)])
+    cmd = [
         'singularity', 'run', '--nv', '-u',
         '--bind', f"{settings['tmp_pred_path']}:/output,"
                   f"{settings['val_rgb_path']}:/input",
